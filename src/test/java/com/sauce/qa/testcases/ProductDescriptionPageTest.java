db27bfcefb7dc7228ebd8e8fc5b8fbb255eb8626
Athira Ramachandran <athirar9559@gmail.com>
2021-08-25 20:25:34 +0400

    Changed pom.xml and edited ProductDesciption.java class

--- src/test/java/com/sauce/qa/testcases/ProductDescriptionPageTest.java
+++ src/test/java/com/sauce/qa/testcases/ProductDescriptionPageTest.java
@@ -11,57 +11,57 @@ import com.sauce.qa.pages.ProductDesciptionPage;
 import com.sauce.qa.pages.ProductListPage;
 
 public class ProductDescriptionPageTest extends TestBase {
-	
+
 	ProductListPage productList;
 	LoginPage loginPage;
 	ProductDesciptionPage productDesc;
-	
+
 	public ProductDescriptionPageTest() {
 		super();
 	}
-	
+
 	@BeforeClass
 	public void setUp() {
-		
+
 		initialization();
-//		loginPage=new LoginPage();
-//		productList =new ProductListPage();
-//		productDesc=new ProductDesciptionPage();
-//		
-		productList=loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
-		productDesc= productList.selectProduct(prop.getProperty("productName"));
+		loginPage = new LoginPage();
+		productList = new ProductListPage();
+		productDesc = new ProductDesciptionPage();
+
+		productList = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		productDesc = productList.selectProduct(prop.getProperty("productName"));
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		driver.close();
 	}
 
-	
-	
-	@Test(priority=1)
+	@Test(priority = 1)
 	public void validateImageDisplayedTest() {
-		Boolean f=productDesc.validateImageDisplayed(prop.getProperty("productName"));
+		Boolean f = productDesc.validateImageDisplayed(prop.getProperty("productName"));
 		Assert.assertTrue(f);
 		System.out.println("Product image is displayed");
-				}
-	
-	@Test(priority=3)
+	}
+
+	@Test(priority = 3)
 	public void validateAddToCartTest() {
 		Boolean f;
-		f=productDesc.validateAddToCart();
+		f = productDesc.validateAddToCart();
 		Assert.assertTrue(f);
 	}
-	@Test(priority=4)
+
+	@Test(priority = 4)
 	public void removeFromCartTest() {
-		Boolean f=productDesc.validateRemoveFromCart();
+		Boolean f = productDesc.validateRemoveFromCart();
 		Assert.assertTrue(f);
 	}
-	@Test(priority=5)
+
+	@Test(priority = 5)
 	public void validateBackToProductsTest() {
-		String actualTitle="Swag Labs";
-		productList=productDesc.validateBackToProducts();
-		String title=driver.getTitle();
+		String actualTitle = "Swag Labs";
+		productList = productDesc.validateBackToProducts();
+		String title = driver.getTitle();
 		System.out.println("title displayed is " + title);
 		if (actualTitle.equalsIgnoreCase(title)) {
 			System.out.println("Page title is as expected");
@@ -70,15 +70,17 @@ public class ProductDescriptionPageTest extends TestBase {
 		}
 		driver.navigate().back();
 	}
-	@Test(priority=6)
+
+	@Test(priority = 6)
 	public void validateYourCartTest() {
-		boolean f =productDesc.validateYourCartPage();
+		boolean f = productDesc.validateYourCartPage();
 		Assert.assertTrue(f);
-		driver.navigate().back();		
+		driver.navigate().back();
 	}
-	@Test(priority=2)
+
+	@Test(priority = 2)
 	public void validateProductTitleTest() {
-		boolean f =productDesc.validateProductTitle(prop.getProperty("productName"));
+		boolean f = productDesc.validateProductTitle(prop.getProperty("productName"));
 		Assert.assertTrue(f);
 	}
 
